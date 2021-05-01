9b531f8c708e35b6824f0bc82f84ad46644f39d5
홍은표 <hackg179@gmail.com>
2021-05-01 16:52:59 +0900

    ㅣㅡ;ㅡ

--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -1,5 +1,34 @@
 
 
 #문제1
+print ("Hello World")
 
-롤홀홀홀홀홇
+#문제2
+print("Mary's cosmetics")
+
+#문제3
+print('신씨가 소리질렀다."도둑이야"')
+
+#문제4:엔터위에 원화기호\
+print("C:\Windows")
+
+#문제5:
+print("안녕하세요. \n만나서\t\t반갑습니다.")
+   # 설명 : \n : 줄바꿈  \t : 탭[들여쓰기]
+
+#문제6: ,[쉽표] 구분짓기
+print('"오늘은" , "일요일"')
+
+#문제7: ;[세미콜론]
+print("naver;kakao:samsung")
+
+#문제8
+print("naver/kakao/samsung")
+
+#문제9: end='' 끝마침을 공백으로 함
+print("first" , end=' ' );print("second")
+
+#문제10:
+print(5/3)
+
+#문제11:
