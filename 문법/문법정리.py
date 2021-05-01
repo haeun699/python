a1591abae7680cdd00da6dec73c3f29d8325f0e9
홍은표 <hackg179@gmail.com>
2021-05-01 17:40:52 +0900

    ㅣㅡ;ㅡ

---
+++ 문법/문법정리.py
@@ -0,0 +1,21 @@
+
+#1. print() 함수
+#   정의 : 코드 출력
+#   사용방법 : print("안녕하세요")
+
+#2. type() int() str() float() 함수
+#   정의 :
+      #type()  :데이터 타입을 확인할 수 있는 함수
+      #int()   :숫자나 문자열을 정수형으로 변환
+      #str()   :숫자를 문자열로 변환하는 함수
+      #float() :숫자나 문자열을 실수형 (Float)으로 변환
+#   사용방법 :
+      #type()  : a = 128 print( type(a) )
+      #int()   : year = "2020" print( int ("2020")) print( int ("2020") +1 ) print( int ("2020") +2 )
+      #str()   : num = 100 num_str = str( num ) print(num_str)
+      #float() : 문자열 = " 15.79" 실수형 = float( 문자열 ) print(실수형)
+
+#3. 인덱싱 이란
+#   정의 : 문자열이나 리스트에 번호를 부여하는것
+
+#3.
---
+++ 문제300/문제21~50.py
@@ -0,0 +1,38 @@
+
+#문제21 : 인덱싱 [ 문자순서] : 0번시작
+letters = 'python'
+print( letters[0] , letters[2] )
+
+#문제22 : 슬라이싱[ 시작번호 , 끝번호 , 단위 ]
+  # 시작번호부터 끝번호 전까지의 문자 출력
+license_plate = "24rk 2210"
+print( license_plate[4:8] )
+print( license_plate[-4: ] )
+
+#문제23 :
+string = "홀짝홀짝홀짝"
+print( string[ 0 : 6 : 2 ]) # 0부터 6전까지 2단위로 이동 [ 0 2 4 ]
+
+#문제24 :
+string = "PYTHON"
+print( string[ : : -1] ) # 뒤에서부터 시작
+
+#문제25 : replace( "기존문자" , "새로운문자" ) 교체 함수
+phone_number = "010-1111-2222"
+phone_number1 = phone_number.replace( "-" , " " ) # -(하이폰)을 공백으로
+print(phone_number1)
+
+#문제26
+phone_number = "010-1111-2222"
+phone_number1 = phone_number.replace( "-" , "" ) # -(하이폰)을 공백으로
+print(phone_number1)
+
+#문제27
+
+#문제28
+
+#문제29
+
+#문제30
+
+
