79162b9405af5491b5df01bb978633ed6fa9ac69
krsthg <ypseoha@naver.com>
2021-06-27 17:14:34 +0900

    ㅣㅡ;ㅡ

--- COS PRO_2/모의고사/문제1.py
+++ COS PRO_2/모의고사/문제1.py
@@ -1,12 +1,20 @@
-def func_a(k):
-    sum = 0
+# 1-m까지 누적합계
+# 1 - n-1까지 누적합계
+# 1단계 - 2단계 의 결과값
+def func_a(k):    # m -> k   # n -1 -> k
+    sum = 0 # 누적합계 변수
     for i in range(k+1):
+    # for 변수 in range( ~~ 전까지 ) : 변수는 1부터 ~~ 전까지 1씩 증가하면서 반복
+         # i는 1 부터 k 전까지 반복
+         # i 는 1부터 k+1 전까지 반복
         sum +=i
 
     return sum
 
 def solution(n, m):
-    sum_to_m = func_a(m)
-    sum_to_n = func_a(n-1)
-    answer = sum_to_m - sum_to_n
+    sum_to_m = func_a(m)          # 1단계
+    sum_to_n = func_a(n-1)        # 2단계
+    answer = sum_to_m - sum_to_n  # 3단계
     return answer
+
+print( solution(5,10))
--- COS PRO_2/모의고사/문제4.py
+++ COS PRO_2/모의고사/문제4.py
@@ -1,14 +1,16 @@
 def solution(scores):
     grade_counter = [0 for i in range(5)]
-    for x in scores:
-        if x >=85:
-            grade_counter[0] += 1
-        elif x >=70:
-            grade_counter[1] += 1
-        elif x >=55:
-            grade_counter[2] += 1
-        elif x >=40:
-            grade_counter[3] += 1
+    # grade_counter : 학점목록에 학점[A-F] 5개를 0으로 설정
+
+    for x in scores: # 점수목록에서 하나씩 x에 대입
+        if x >=85: # x가 85점 이상이면
+            grade_counter[0] += 1  # 학점목록[0] = a학점 한명추가
+        elif x >=70: # x가 70점 이상이면
+            grade_counter[1] += 1   # 학점목록[1] = b학점 한명추가
+        elif x >=55: # x가 55점 이상이면
+            grade_counter[2] += 1    # 학점목록[2] = c학점 한명추가
+        elif x >=40: # x가 40점 이상이면
+            grade_counter[3] += 1    # 학점목록[3] = d학점 한명추가
         else:
-            grade_counter[4] += 1
+            grade_counter[4] += 1     # 학점목록[4] = f학점 한명추가
     return grade_counter
